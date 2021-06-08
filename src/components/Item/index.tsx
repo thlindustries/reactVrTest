c051bc6469ac3ae51cbd64ded5eeb5c02db5d5d7
root <thiago.kraetzer@brainweb.com.br>
2021-06-08 11:27:41 -0300

    Fix: webAr test fix

--- src/components/Item/index.tsx
+++ src/components/Item/index.tsx
@@ -1,6 +1,6 @@
 import { Suspense, useRef } from 'react'
-import { Canvas } from '@react-three/fiber'
 import { OrbitControls, Stage } from '@react-three/drei'
+import { VRCanvas } from '@react-three/xr'
 
 import Caixa from './Caixa';
 import styles from './styles.module.scss';
@@ -11,14 +11,14 @@ export const Item = () => {
   return (
     <div className={styles.model}>
       <p>Teste</p>
-      <Canvas shadows dpr={[1, 2]} camera={{ fov: 50 }}>
+      <VRCanvas shadows dpr={[1, 2]} camera={{ fov: 50 }}>
         <Suspense fallback={null}>
           <Stage controls={ref} preset="rembrandt" intensity={1} environment="city">
             <Caixa />
           </Stage>
         </Suspense>
         <OrbitControls ref={ref} autoRotate />
-      </Canvas>
+      </VRCanvas>
     </div>
   );
 }
