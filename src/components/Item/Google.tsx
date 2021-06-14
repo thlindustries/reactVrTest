9e91b79476a9f57b2a8d232b85d8639b36d51bb2
root <thiago.kraetzer@brainweb.com.br>
2021-06-14 20:21:19 -0300

    Update: fixing

--- src/components/Item/Google.tsx
+++ src/components/Item/Google.tsx
@@ -39,7 +39,7 @@ export const Google = ({ src, ios, autoAR = false, cardMsg }: any) => {
         shadow-softness={0}
         exposure={1.1}
         camera-orbit="90deg 90deg 1.5m"
-        ar-modes="webxr scene-viewer"
+        ar-modes="webxr"
         id="model-viewer-test"
         ar-status="session-started"
       >
